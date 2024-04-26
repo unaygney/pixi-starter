8fc7cc24d594879cc0b2dcc9e5019c0ee4991219
Guney Unay <guneymarketing17@hotmail.com>
2024-04-26 15:49:13 +0300

    :wrench: Added webpack.config.js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// import * as PIXI from "pixi.js";
+
+// // Renderer oluşturun
+// const renderer = new PIXI.Renderer({
+//   width: 800,
+//   height: 600,
+//   backgroundColor: 0x1099bb,
+// });
+
+// // View'ı belirtilen div'e yerleştirin
+// document.getElementById("pixi-container").appendChild(renderer.view);
+
+// // Ticker ve stage oluşturun
+// const stage = new PIXI.Container();
+// const ticker = new PIXI.Ticker();
+
+// // Ticker'a bir güncelleme fonksiyonu ekleyin
+// ticker.add(() => {
+//   renderer.render(stage);
+// });
+
+// // Ticker'ı başlatın
+// ticker.start();
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+};
